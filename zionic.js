613155fa37418553e867521d50943678f86ea1b2
alvisjiang <alvisjiang@wakfo.com>
2018-06-14 19:27:21 +0800

    Fix a typo
    json -> JSON

--- zionic.js
+++ zionic.js
@@ -1,6 +1,6 @@
 var nic = {};
 function zion(viewName) {
-    let nativeFuncs = json.parse(prompt("zionic://getFuncs", viewName));
+    let nativeFuncs = JSON.parse(prompt("zionic://getFuncs", viewName));
     for (let i = 0; i < nativeFuncs.length; i++) {
         let funcName = nativeFuncs[i];
         nic[funcName] = params => {
